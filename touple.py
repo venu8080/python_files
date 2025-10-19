f190192f25681e35862fae1c8937a61112992271
venu8080 <24B11CS271@adityauniversity.in>
2025-10-19 21:38:51 +0530

    Add files via upload

---
+++ touple.py
@@ -0,0 +1,9 @@
+marks = (85, 90, 78, 92, 100)
+print("Student marks:", marks)
+
+print("First subject mark:", marks[0])
+print("Last subject mark:", marks[-1])
+print("Number of subjects:", len(marks))
+print("All marks:")
+for mark in marks:
+    print(mark)
